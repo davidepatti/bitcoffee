47f9d6a4a4ca333391258dea5d4fb263c7d1ab5d
davidepatti <xedivad@gmail.com>
2021-04-21 19:52:47 +0200

    Big decimal version

--- src/FieldElement.java
+++ src/FieldElement.java
@@ -1,13 +1,15 @@
+import java.math.BigInteger;
 import java.util.Objects;
 
 // TODO: mutable?
 public final class FieldElement {
-    private final long num;
-    private final long prime;
+    private final BigInteger num;
+    private final BigInteger prime;
 
-    public FieldElement(long num, long prime) {
-        if (num>= prime || num <0) {
-            System.out.println(" Element "+num+ " not in 0.."+(prime-1));
+    public FieldElement(BigInteger num, BigInteger prime) {
+
+        if (num.compareTo(prime)>=0 || num.compareTo(BigInteger.ZERO) <0) {
+            System.out.println(" Element "+num+ " not in 0.."+(prime.subtract(BigInteger.ONE)));
             System.exit(-1);
         }
         this.num =num;
@@ -20,41 +22,37 @@ public final class FieldElement {
     }
 
     public FieldElement plus(FieldElement other) {
-        if (this.prime!=other.prime) {
+        if (this.prime.compareTo(other.prime)!=0) {
             System.out.println("Different fields "+this.prime+" and "+other.prime);
             System.exit(-1);
         }
 
-        long sum = (this.num+ other.num)%this.prime;
+        var sum = (this.num.add(other.num)).mod(this.prime);
         return new FieldElement(sum,this.prime);
     }
 
     public FieldElement mul(FieldElement other) {
 
-        if (this.prime!=other.prime) {
+        if (this.prime.compareTo(other.prime)!=0) {
             System.out.println("Different fields");
             System.exit(-1);
         }
 
-        long product = (this.num* other.num) % this.prime;
+        var product = (this.num.multiply(other.num)).mod(this.prime);
 
         return new FieldElement(product,this.prime);
     }
 
-    public FieldElement pow(int exponent) {
-        long n = this.num;
-        while (--exponent>0) n = n*this.num;
-        n = n%this.prime;
-        return new FieldElement(n,this.prime);
-    }
     public FieldElement pow2(int exponent) {
-        long n = exponent % (this.prime-1);
-        double base = this.num;
+        var exp = BigInteger.valueOf(exponent);
+        exp = exp.mod(this.prime.subtract(BigInteger.ONE));
 
-        long num =  (long) Math.pow(base,(double)n);
-        num = num%this.prime;
+        var num_res  = this.num.modPow(exp,this.prime);
 
-        return new FieldElement(num,this.prime);
+        //long num =  (long) Math.pow(base,(double)n);
+        //num = num%this.prime;
+
+        return new FieldElement(num_res,this.prime);
     }
 
     @Override
@@ -67,7 +65,7 @@ public final class FieldElement {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         FieldElement that = (FieldElement) o;
-        return num == that.num && prime == that.prime;
+        return num.equals(that.num) && prime.equals(that.prime);
     }
 
     @Override
--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,34 @@
+import java.math.BigInteger;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
 
         // test mult  /////////////////////////////////
-        var a = new FieldElement(3,13);
-        var b = new FieldElement(12,13);
-        var c = new FieldElement(10,13);
+        var a = new FieldElement(BigInteger.valueOf(3),BigInteger.valueOf(13));
+        var b = new FieldElement(BigInteger.valueOf(12),BigInteger.valueOf(13));
+        var c = new FieldElement(BigInteger.valueOf(10),BigInteger.valueOf(13));
 
         var res = a.mul(b);
         // test ok 10,13
         System.out.println(res.equals(c));
         //////////////////////////////////////////////////
 
-        // ok if result 1,13
-        System.out.println(a+" pow 3 = "+a.pow(4));
-
         // power to p-1 -> 1
-        System.out.println(a+" pow 3 = "+a.pow2(101));
+        System.out.println(a+" pow 3 = "+a.pow2(151));
 
 
+        a = new FieldElement(BigInteger.TWO,BigInteger.valueOf(19));
+        b = new FieldElement(BigInteger.valueOf(7),BigInteger.valueOf(19));
 
+        // 2/7 -> 2* 7 (19-2)
+        c = a.mul(b.pow2(19-2));
+
+        System.out.println(c);
+
+        a = new FieldElement(BigInteger.valueOf(7),BigInteger.valueOf(19));
+        b = new FieldElement(BigInteger.valueOf(5),BigInteger.valueOf(19));
+        System.out.println(a.mul(b.pow2(19-2)));
     }
 }
