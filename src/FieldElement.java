42669a9000699d675ef1368bb014ee9420346540
davidepatti <xedivad@gmail.com>
2021-04-21 18:35:35 +0200

    Draft add, mult, pow

--- src/FieldElement.java
+++ src/FieldElement.java
@@ -1,9 +1,10 @@
 import java.util.Objects;
 
+// TODO: mutable?
 public class FieldElement {
-    int num;
-    int prime;
-    public FieldElement(int num, int prime) {
+    long num;
+    long prime;
+    public FieldElement(long num, long prime) {
         if (num>= prime || num <0) {
             System.out.println(" Element "+num+ " not in 0.."+(prime-1));
             System.exit(-1);
@@ -12,12 +13,52 @@ public class FieldElement {
         this.prime = prime;
     }
 
+    public FieldElement(FieldElement other) {
+        this.num = other.num;
+        this.prime = other.prime;
+    }
+
+    public FieldElement add(FieldElement other) {
+        if (this.prime!=other.prime) {
+            System.out.println("Different fields "+this.prime+" and "+other.prime);
+            System.exit(-1);
+        }
+
+        long sum = (this.num+ other.num)%this.prime;
+        return new FieldElement(sum,this.prime);
+    }
+
+    public FieldElement mul(FieldElement other) {
+
+        if (this.prime!=other.prime) {
+            System.out.println("Different fields");
+            System.exit(-1);
+        }
+
+        long product = (this.num* other.num) % this.prime;
+
+        return new FieldElement(product,this.prime);
+    }
+
+    public FieldElement pow(int exponent) {
+        long n = this.num;
+        while (--exponent>0) n = n*this.num;
+        n = n%this.prime;
+        return new FieldElement(n,this.prime);
+    }
+    public FieldElement pow2(int exponent) {
+        long n = exponent % (this.prime-1);
+        double base = this.num;
+
+        long num =  (long) Math.pow(base,(double)n);
+        num = num%this.prime;
+
+        return new FieldElement(num,this.prime);
+    }
+
     @Override
     public String toString() {
-        return "FieldElement{" +
-                "num=" + num +
-                ", prime=" + prime +
-                '}';
+        return "FieldElement{" + num + "," + prime + '}';
     }
 
     @Override
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,24 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        // test mult  /////////////////////////////////
+        var a = new FieldElement(3,13);
+        var b = new FieldElement(12,13);
+        var c = new FieldElement(10,13);
+
+        var res = a.mul(b);
+        // test ok 10,13
+        System.out.println(res.equals(c));
+        //////////////////////////////////////////////////
+
+        // ok if result 1,13
+        System.out.println(a+" pow 3 = "+a.pow(4));
+
+        // power to p-1 -> 1
+        System.out.println(a+" pow 3 = "+a.pow2(101));
+
+
+
     }
 }
