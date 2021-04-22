8a9fe0ff6760aecf83f45493bae941e3e3ce596d
davidepatti <xedivad@gmail.com>
2021-04-22 17:50:19 +0200

    Implemented all point addition cases

--- src/Test.java
+++ src/Test.java
@@ -1,3 +1,5 @@
+import java.math.BigInteger;
+
 public class Test {
 
     public static void main(String[] args) {
@@ -45,5 +47,37 @@ public class Test {
         target = new FieldElement(9,19);
         res = a.div(b);
         System.out.println(a+" div "+b+" = "+res+ " "+res.equals(target));
+
+
+        ///////////// Elliptic curve
+
+        // infinity point is also identity point, result should not change
+        var p1 = new Point(-1,-1,5,7);
+        var inf = new Point(Point.BIGINF,Point.BIGINF, BigInteger.valueOf(5),BigInteger.valueOf(7));
+        var point_target = p1;
+        var point_result = p1.add(inf);
+        System.out.println(p1+" + Inf = "+point_result+" "+point_result.equals(point_target));
+
+        var p2 = new Point(-1,1,5,7);
+        point_result = p2.add(inf);
+        point_target = p2;
+        System.out.println(p2+" + Inf = "+point_result+" "+point_result.equals(point_target));
+
+        point_result = p1.add(p2);
+        point_target = inf;
+        System.out.println(p1+"+"+p2+" = "+point_result+" "+point_result.equals(point_target));
+
+
+        p1 = new Point(3,7,5,7);
+        p2 = new Point(-1,-1,5,7);
+        point_target = new Point(2,-5,5,7);
+        point_result = p1.add(p2);
+        System.out.println(p1+" + "+p2+" = "+point_result+ " "+point_result.equals(point_target));
+
+        point_result = p2.add(p2);
+        point_target = new Point(18,77,5,7);
+        System.out.println(p2+" + "+p2+" = "+point_result+ " "+point_result.equals(point_target));
+
+
     }
 }
