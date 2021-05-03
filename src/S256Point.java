693503ad1c2247eb37246da8a37fc9a99c40806e
davidepatti <xedivad@gmail.com>
2021-05-03 19:53:50 +0200

    Added draft of signature function

--- src/S256Point.java
+++ src/S256Point.java
@@ -13,8 +13,23 @@ public class S256Point extends FieldElementPoint{
         this.y = y;
     }
 
+    public S256Point(FieldElementPoint other) {
+        super(other);
+
+        this.x = other.getX().getNum();
+        this.y = other.getY().getNum();
+    }
+
     @Override
     public String toString() {
         return "S256Point("+x+","+y+")";
+    }
+
+    public boolean verify(BigInteger z, Signature sig) {
+        var s_inv = sig.s.modPow(Secp256k1.N.subtract(BigInteger.TWO), Secp256k1.N);
+        var u = z.multiply(s_inv.mod(Secp256k1.N));
+        var v = sig.r.multiply(s_inv.mod(Secp256k1.N));
+        var total = Secp256k1.G.multiply_bin(u).add(this.multiply_bin(v));
+        return  total.getX().getNum().equals(sig.r);
     }
 }
---
+++ src/Signature.java
@@ -0,0 +1,13 @@
+import java.math.BigInteger;
+
+public class Signature {
+    final BigInteger r;
+    final BigInteger s;
+
+    public Signature(BigInteger r, BigInteger s) {
+        this.r = r;
+        this.s = s;
+    }
+
+
+}
