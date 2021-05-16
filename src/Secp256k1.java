e6780b41c59e554e68920fac508cec0c787e3879
davidepatti <xedivad@gmail.com>
2021-05-16 16:09:04 +0200

    Ripemd160 and Baseencode58

--- src/Secp256k1.java
+++ src/Secp256k1.java
@@ -1,7 +1,9 @@
+import java.io.ByteArrayOutputStream;
 import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 public class Secp256k1 {
     public static final BigInteger a = BigInteger.ZERO;
@@ -12,8 +14,7 @@ public class Secp256k1 {
     public static final BigInteger N = new BigInteger("fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141",16);
     public static final S256Point G = new S256Point(Gx,Gy);
 
-
-    public static final byte[] hash256(String message) {
+    public static final byte[] sha256(byte[] b) {
         MessageDigest digester = null;
         try {
             digester = MessageDigest.getInstance("SHA-256");
@@ -22,9 +23,75 @@ public class Secp256k1 {
             e.printStackTrace();
         }
 
-        byte[] hash = digester.digest(digester.digest(message.getBytes(StandardCharsets.UTF_8)));
-
+        byte[] hash = digester.digest(b);
         return hash;
+    }
+
+    public static final byte[] hash256(byte[] b) {
+        return sha256(sha256(b));
+    }
+
+    public static final byte[] ripemd160(byte[] b) {
+        MessageDigest digester = null;
+        try {
+            digester = MessageDigest.getInstance("RIPEMD160");
+        } catch (
+                NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        byte[] hash = digester.digest(b);
+        return hash;
+    }
+
+    public static final byte[] hash160(byte[] b) {
+        return ripemd160(sha256(b));
+    }
+
+    public static final byte[] hash256(String message) {
+        return hash256(message.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public static String encodeBase58(byte[] s) {
+        String BASE58_AlPHABET = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+        int count = 0;
+        for (byte b: s) {
+            if (b==0) count++;
+            else break;
+        }
+        // TODO: fix this
+        if (count>1) {
+            System.out.println("Warning on encodeBase58 leading zero' byets");
+            System.exit(-1);
+        }
+        var num = new BigInteger(1,s);
+        String prefix = "";
+        for (int c=0;c<count;c++)
+            prefix = "1"+prefix;
+
+        String result = "";
+        BigInteger[] num_mod = new BigInteger[2];
+        while (num.compareTo(BigInteger.ZERO)>0) {
+            num_mod = num.divideAndRemainder(BigInteger.valueOf(58));
+            num = num_mod[0];
+            int mod = num_mod[1].intValue();
+            result = BASE58_AlPHABET.charAt(mod)+result;
+        }
+        //return prefix+result;
+        return result;
+    }
+
+    public static String encodeBase58Checksum(byte[] b) {
+        var hash_b = hash256(b);
+        var bos = new ByteArrayOutputStream();
+        bos.writeBytes(b);
+        bos.write(hash_b[0]);
+        bos.write(hash_b[1]);
+        bos.write(hash_b[2]);
+        bos.write(hash_b[3]);
+        var res = bos.toByteArray();
+        var res_enc = encodeBase58(res);
+        return res_enc;
     }
 
 }
--- src/TestSerialization.java
+++ src/TestSerialization.java
@@ -53,5 +53,15 @@ public class TestSerialization {
         System.out.println(der_str.equals(target_der));
 
 
+        var a1 = new BigInteger("7c076ff316692a3d7eb3c3bb0f8b1488cf72e1afcd929e29307032997a838a3d",16);
+        System.out.println(Secp256k1.encodeBase58(a1.toByteArray()));
+
+        var a2 = new BigInteger("eff69ef2b1bd93a66ed5219add4fb51e11a840f404876325a1e8ffe0529a2c",16);
+        System.out.println(Secp256k1.encodeBase58(a2.toByteArray()));
+
+        var a3 = new BigInteger("c7207fee197d27c618aea621406f6bf5ef6fca38681d82b2f06fddbdce6feab6",16);
+        System.out.println(Secp256k1.encodeBase58(a3.toByteArray()));
+
+
     }
 }
