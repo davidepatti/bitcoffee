52a0bc07e495e95dd61189b2910a2e965d2edfc6
davidepatti <xedivad@gmail.com>
2021-09-21 16:17:32 +0200

    Added equals overriding

--- src/ScriptCmd.java
+++ src/ScriptCmd.java
@@ -1,23 +1,26 @@
+import java.util.Arrays;
+import java.util.Objects;
+
 public class ScriptCmd {
     public final byte[] value;
-    public final OpCode type;
+    public final ScriptCmdType type;
 
 
-    public ScriptCmd(OpCode type, byte [] value) {
+    public ScriptCmd(ScriptCmdType type, byte [] value) {
         this.type = type;
         this.value = value;
     }
 
-    public ScriptCmd(OpCode type) {
+    public ScriptCmd(ScriptCmdType type) {
         // special operations that need reading the next bytes cannot be
         // initialized with opcode only
-        assert type!=OpCode.DATA;
-        assert type!=OpCode.OP_PUSHDATA1;
-        assert type!=OpCode.OP_PUSHDATA2;
-        assert type!=OpCode.OP_PUSHDATA4;
+        assert type!= ScriptCmdType.DATA;
+        assert type!= ScriptCmdType.OP_PUSHDATA1;
+        assert type!= ScriptCmdType.OP_PUSHDATA2;
+        assert type!= ScriptCmdType.OP_PUSHDATA4;
 
         this.type = type;
-        this.value = new byte[] { (byte)type.getOpcode() };
+        this.value = new byte[] { (byte)type.getValue() };
     }
 
     @Override
@@ -26,4 +29,18 @@ public class ScriptCmd {
         return "\n{" + CryptoKit.bytesToHexString(value) + ", " + type + "}";
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ScriptCmd scriptCmd = (ScriptCmd) o;
+        return Arrays.equals(value, scriptCmd.value) && type == scriptCmd.type;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(type);
+        result = 31 * result + Arrays.hashCode(value);
+        return result;
+    }
 }
