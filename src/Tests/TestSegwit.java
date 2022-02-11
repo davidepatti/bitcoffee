ee3da6aa5c3a8573757967f640f1b7534d1ab2d8
davidepatti <xedivad@gmail.com>
2022-02-11 17:58:44 +0100

    Working tests p2wpk

--- src/Tests/TestSegwit.java
+++ src/Tests/TestSegwit.java
@@ -8,6 +8,7 @@ public class TestSegwit {
         Test.__BEGIN_TEST("verify bitcoffee.Tx");
         var tx_id = "d869f854e1f8788bcff294cc83b280942a8c728de71eb709a2c29d10bfe21b7c";
         var tx = TxFetcher.fetch(tx_id,true,true);
+        System.out.println(tx);
         Test.check("p2wpk","tx id:"+tx_id,true,tx.verify());
 
         tx_id = "c586389e5e4b3acb9d6c8be1c19ae8ab2795397633176f5a6442a261bbdefc3a";
